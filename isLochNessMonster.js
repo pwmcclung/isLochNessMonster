076c5d547967aaad5d2a45e6951ff13c0e182abf
Patrick <78838481+pwmcclung@users.noreply.github.com>
2022-05-10 07:21:17 -0600

    Create isLochNessMonster.js

---
+++ isLochNessMonster.js
@@ -0,0 +1,11 @@
+function isLockNessMonster(s) {
+  //FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+  let one = "tree fiddy"
+  let two = "3.50"
+  let three = "three fifty"
+  if (s.includes(one)|| s.includes(two)|| s.includes(three)){
+    return true
+  }else{
+    return false
+  }
+}
